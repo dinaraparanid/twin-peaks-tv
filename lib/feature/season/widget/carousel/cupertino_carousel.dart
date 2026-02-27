c9daa47c525b6279679ae58246cb3e04bd25897e
Paranid5 <dinaraparanid@gmail.com>
2026-02-27 20:34:09 +0300

    tvos carousel

--- lib/feature/season/widget/carousel/cupertino_carousel.dart
+++ lib/feature/season/widget/carousel/cupertino_carousel.dart
@@ -56,63 +56,61 @@ final class CupertinoCarousel extends StatelessWidget {
       settings: AppLiquidGlass.defaultSettings(context),
       child: LiquidGlass(
         shape: LiquidRoundedRectangle(borderRadius: 24.r),
-        child: Padding(
+        child: TvScrollCarouselPager(
+          height: 32.s,
+          capacity: 5,
           padding: EdgeInsets.symmetric(horizontal: 12.s, vertical: 8.s),
-          child: TvScrollCarouselPager(
-            height: 20.s,
-            capacity: 5,
-            itemCount: season.thumbnailUrls.length,
-            controller: context.seasonBloc.carouselController,
-            focusScopeNode: context.seasonBloc.carouselNode,
-            separatorBuilder: (context, index, selectedIndex, visibleIndices) {
-              final size = _itemSize(
-                index: index,
-                selectedIndex: selectedIndex,
-                visibleIndices: visibleIndices,
-              );
+          itemCount: season.thumbnailUrls.length,
+          controller: context.seasonBloc.carouselController,
+          focusScopeNode: context.seasonBloc.carouselNode,
+          separatorBuilder: (context, index, selectedIndex, visibleIndices) {
+            final size = _itemSize(
+              index: index,
+              selectedIndex: selectedIndex,
+              visibleIndices: visibleIndices,
+            );
 
-              return SizedBox(width: size > 0 ? 8.s : 0);
-            },
-            viewportAlignment: (context, selectedIndex, visibleIndices) {
-              return selectedIndex == visibleIndices.$1 ? 0.5 : null;
-            },
-            onDown: (_, _) {
-              context.seasonBloc.add(const RequestFocusOnCast());
-              return KeyEventResult.handled;
-            },
-            onLeft: (_, _, hasReachedBoundary) {
-              if (hasReachedBoundary) {
-                context.seasonBloc.add(const RequestFocusOnDescription());
-              }
+            return SizedBox(width: size > 0 ? 8.s : 0);
+          },
+          viewportAlignment: (context, selectedIndex, visibleIndices) {
+            return selectedIndex == visibleIndices.$1 ? 0.5 : null;
+          },
+          onDown: (_, _) {
+            context.seasonBloc.add(const RequestFocusOnCast());
+            return KeyEventResult.handled;
+          },
+          onLeft: (_, _, hasReachedBoundary) {
+            if (hasReachedBoundary) {
+              context.seasonBloc.add(const RequestFocusOnDescription());
+            }
 
-              return KeyEventResult.handled;
-            },
-            onRight: (_, _, _) => KeyEventResult.handled,
-            itemBuilder:
-                (context, index, selectedIndex, visibleIndices, isFocused) {
-                  final size = _itemSize(
-                    index: index,
-                    selectedIndex: selectedIndex,
-                    visibleIndices: visibleIndices,
-                  );
+            return KeyEventResult.handled;
+          },
+          onRight: (_, _, _) => KeyEventResult.handled,
+          itemBuilder:
+              (context, index, selectedIndex, visibleIndices, isFocused) {
+                final size = _itemSize(
+                  index: index,
+                  selectedIndex: selectedIndex,
+                  visibleIndices: visibleIndices,
+                );
 
-                  final isSelected = index == selectedIndex;
+                final isSelected = index == selectedIndex;
 
-                  return AnimatedContainer(
-                    duration: const Duration(milliseconds: 200),
-                    width: size,
-                    height: size,
-                    decoration: BoxDecoration(
-                      shape: BoxShape.circle,
-                      color: switch ((isSelected, isFocused)) {
-                        (false, _) => carouselColors.content,
-                        (true, true) => carouselColors.focusedContent,
-                        (true, false) => carouselColors.selectedContent,
-                      },
-                    ),
-                  );
-                },
-          ),
+                return AnimatedContainer(
+                  duration: const Duration(milliseconds: 200),
+                  width: size,
+                  height: size,
+                  decoration: BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: switch ((isSelected, isFocused)) {
+                      (false, _) => carouselColors.content,
+                      (true, true) => carouselColors.focusedContent,
+                      (true, false) => carouselColors.selectedContent,
+                    },
+                  ),
+                );
+              },
         ),
       ),
     );
