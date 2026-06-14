2d0388a2e3f9ef06709322bbbc5caea5de29747d
paranid5 <dinaraparanid@gmail.com>
2026-06-14 15:30:22 +0300

    tizen fixes

--- lib/feature/encyclopedia/encyclopedia_screen.dart
+++ lib/feature/encyclopedia/encyclopedia_screen.dart
@@ -10,6 +10,7 @@ import 'package:twin_peaks_tv/feature/encyclopedia/bloc/bloc.dart';
 import 'package:twin_peaks_tv/feature/encyclopedia/widget/browse/browse_block.dart';
 import 'package:twin_peaks_tv/feature/encyclopedia/widget/recent/recent_block.dart';
 import 'package:twin_peaks_tv/feature/encyclopedia/widget/search_bar.dart';
+import 'package:twin_peaks_tv/feature/main/main_screen.dart';
 
 @RoutePage()
 final class EncyclopediaScreen extends StatelessWidget {
@@ -23,47 +24,55 @@ final class EncyclopediaScreen extends StatelessWidget {
         buildWhen: distinctState((s) {
           return (s.recentCharacters, s.browseCharacters, s.language);
         }),
-        builder: (context, state) => CustomScrollView(
-          slivers: [
-            SliverPadding(
-              padding: EdgeInsets.only(
-                top: switch (AppPlatform.isTvOS) {
-                  true => 24.s + 48.s,
-                  false => 24.s,
-                },
-                left: 24.s,
-                right: 24.s,
-              ),
-              sliver: SliverToBoxAdapter(
-                child: SearchBar(currentLocale: state.language?.toLocale()),
-              ),
-            ),
-
-            SliverToBoxAdapter(child: SizedBox(height: 32.s)),
-
-            switch ((state.recentCharacters, state.browseCharacters)) {
-              (Data(value: final recents), Data(value: final browse)) =>
-                MultiSliver(
-                  children: [
-                    if (recents.isNotEmpty) ...[
-                      SliverToBoxAdapter(
-                        child: RecentBlock(characters: recents),
-                      ),
-                      SliverToBoxAdapter(child: SizedBox(height: 12.s)),
-                    ],
-
-                    SliverToBoxAdapter(child: SizedBox(height: 16.s)),
-                    SliverBrowseBlock(characters: browse),
-                  ],
-                ),
-
-              (_, _) => SliverFillRemaining(
-                child: Stack(
-                  children: [Align(child: AppLoader(radius: 20.s))],
-                ),
-              ),
+        builder: (context, state) => Padding(
+          padding: EdgeInsets.only(
+            left: switch (AppPlatform.isTizen) {
+              true => MainScreen.oneUiMenuConstraints.minWidth,
+              false => 0.s,
             },
-          ],
+          ),
+          child: CustomScrollView(
+            slivers: [
+              SliverPadding(
+                padding: EdgeInsets.only(
+                  top: switch (AppPlatform.isTvOS) {
+                    true => 24.s + 48.s,
+                    false => 24.s,
+                  },
+                  left: 24.s,
+                  right: 24.s,
+                ),
+                sliver: SliverToBoxAdapter(
+                  child: SearchBar(currentLocale: state.language?.toLocale()),
+                ),
+              ),
+
+              SliverToBoxAdapter(child: SizedBox(height: 32.s)),
+
+              switch ((state.recentCharacters, state.browseCharacters)) {
+                (Data(value: final recents), Data(value: final browse)) =>
+                  MultiSliver(
+                    children: [
+                      if (recents.isNotEmpty) ...[
+                        SliverToBoxAdapter(
+                          child: RecentBlock(characters: recents),
+                        ),
+                        SliverToBoxAdapter(child: SizedBox(height: 12.s)),
+                      ],
+
+                      SliverToBoxAdapter(child: SizedBox(height: 16.s)),
+                      SliverBrowseBlock(characters: browse),
+                    ],
+                  ),
+
+                (_, _) => SliverFillRemaining(
+                  child: Stack(
+                    children: [Align(child: AppLoader(radius: 20.s))],
+                  ),
+                ),
+              },
+            ],
+          ),
         ),
       ),
     );
